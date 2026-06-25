0d848cdf9afa10779783299444079f003ae98bd2
Leonardo <leoavalente@gmail.com>
2026-06-25 19:16:20 -0300

    Update navBarU.jsx

--- montador_de_rpg/src/Components/NavBar/navBarU.jsx
+++ montador_de_rpg/src/Components/NavBar/navBarU.jsx
@@ -35,14 +35,14 @@ export default function NavBar() {
     <nav className={`${styles.navContainer} ${scrolled ? styles.navScrolled : ''}`}>
         <div className={styles.navContents}>
           <div className={styles.logo}>
-            <Link to="/" onClick={() => Location.reload()}><img  src={logo} alt="Logo do Montador de RPG"/></Link>
+            <Link to="/" {/* onClick={() => Location.reload()} */}><img  src={logo} alt="Logo do Montador de RPG"/></Link>
           </div>
           <div className={styles.links}>
             <li><Link to="/" onClick={() => Location.reload()}>Sistemas</Link></li>
             <li><Link to="/" onClick={() => Location.reload()}>Mesa</Link></li>
           </div>
         </div>
-          <button className={styles.btnDouradoCheio} onClick={handleLogout}> Sair </button>
+        <button className={styles.btnDouradoCheio} onClick={handleLogout}> Sair </button>
     </nav>
     </>
   )
